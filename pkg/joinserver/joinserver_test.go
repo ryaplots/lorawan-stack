186bbd5c0767b01344c14f0db994c57dfcf59445
Adrian-Stefan Mares <adrian.mares@thethingsnetwork.org>
2021-09-28 18:14:45 +0200

    js: Use lock for key registry

--- pkg/joinserver/joinserver_test.go
+++ pkg/joinserver/joinserver_test.go
@@ -136,7 +136,10 @@ func TestInvalidJoinRequests(t *testing.T) {
 				if err := devReg.Init(ctx); !a.So(err, should.BeNil) {
 					t.FailNow()
 				}
-				keyReg := &redis.KeyRegistry{Redis: redisClient}
+				keyReg := &redis.KeyRegistry{Redis: redisClient, LockTTL: test.Delay << 10}
+				if err := keyReg.Init(ctx); !a.So(err, should.BeNil) {
+					t.FailNow()
+				}
 				aasReg, aasRegCloseFn := NewRedisApplicationActivationSettingRegistry(ctx)
 				defer aasRegCloseFn()
 
@@ -2034,7 +2037,10 @@ func TestHandleJoin(t *testing.T) {
 				if err := devReg.Init(ctx); !a.So(err, should.BeNil) {
 					t.FailNow()
 				}
-				keyReg := &redis.KeyRegistry{Redis: redisClient}
+				keyReg := &redis.KeyRegistry{Redis: redisClient, LockTTL: test.Delay << 10}
+				if err := keyReg.Init(ctx); !a.So(err, should.BeNil) {
+					t.FailNow()
+				}
 				aasReg, aasRegCloseFn := NewRedisApplicationActivationSettingRegistry(ctx)
 				defer aasRegCloseFn()
 
